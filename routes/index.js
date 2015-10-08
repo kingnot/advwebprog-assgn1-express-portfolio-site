305e8f375db39b64aae799ae1e9ad23a869910cb
kingnot <kingnot0524@gmail.com>
2015-10-07 20:48:22 -0400

    create different views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'FEI WANG',
+                        message: 'My Portfolio'});
+});
+
+/* GET About Me page */
+router.get('/about', function(req, res, next) {
+    res.render('about', { title: 'About Me'});
+});
+
+/* GET Projects page */
+router.get('/project', function(req, res, next) {
+    res.render('project', { title: 'Projects'});
+});
+
+/* GET Services page */
+router.get('/service', function(req, res, next) {
+    res.render('service', { title: 'Services'});
+});
+
+/* GET Contact page */
+router.get('/contact', function(req, res, next) {
+    res.render('contact', { title: 'Contact Me'});
 });
 
 module.exports = router;
